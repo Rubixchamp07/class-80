11d04e3cccd453a3b288cd43c221c60e41750be0
Rubixchamp07 <88152130+Rubixchamp07@users.noreply.github.com>
2021-08-09 10:04:23 -0400

    Update main.js

--- main.js
+++ main.js
@@ -39,8 +39,7 @@ function sorting() {
         console.log(remove_commas);
         document.getElementById("display_name_without_commas").innerHTML = remove_commas;
     }
-
+}
     function new_update() {
         document.getElementById("display_name_without_commas").innerHTML = "<h1>" + name_of_the_student_array + "<h1>";
     }
-}
